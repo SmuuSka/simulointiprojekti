666e8c7475256cddcfffd406988e28072ab693ce
zzokku <93818329+zzokku@users.noreply.github.com>
2023-02-02 17:27:14 +0200

    test

--- src/main/java/com/metropolia/simuryhmaYksi/sorttiasema/simu/model/Palvelutiski.java
+++ src/main/java/com/metropolia/simuryhmaYksi/sorttiasema/simu/model/Palvelutiski.java
@@ -4,6 +4,7 @@ import com.metropolia.simuryhmaYksi.sorttiasema.eduni.distributions.ContinuousGe
 import com.metropolia.simuryhmaYksi.sorttiasema.simu.framework.Tapahtumalista;
 
 public class Palvelutiski extends Palvelupiste {
+    // moro
     public Palvelutiski(ContinuousGenerator generator, Tapahtumalista tapahtumalista, TapahtumanTyyppi tyyppi) {
         super(generator, tapahtumalista, tyyppi);
     }
