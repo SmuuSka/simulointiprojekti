c5c680010ec6e2e61c72462085f252f4724d896d
zzokku <93818329+zzokku@users.noreply.github.com>
2023-02-04 19:32:20 +0200

    kommentti

--- src/main/java/com/metropolia/simuryhmaYksi/sorttiasema/simu/model/Jate.java
+++ src/main/java/com/metropolia/simuryhmaYksi/sorttiasema/simu/model/Jate.java
@@ -5,7 +5,8 @@ public class Jate {
     private Jatelaji jatelaji;
 
     public Jate(){
-
+        // TODO:
+        // Satunnainen lajin ja painon arpominen
     }
 
     public Jate(Jatelaji jatelaji, double maara){
