96e935b51317109c00bc9c9baf91fba8d6acf128
zzokku <93818329+zzokku@users.noreply.github.com>
2023-02-04 18:42:35 +0200

    keskimääräinen jätteenmäärä sekä kommenttien ja koodin siistimistä

--- src/main/java/com/metropolia/simuryhmaYksi/sorttiasema/simu/model/Jatelava.java
+++ src/main/java/com/metropolia/simuryhmaYksi/sorttiasema/simu/model/Jatelava.java
@@ -9,20 +9,21 @@ public class Jatelava extends Palvelupiste {
 
     private double maara;
     private final Jatelaji lavanTyyppi;
+    public static int saapuneet = 0;
 
     public Jatelava(ContinuousGenerator generator, Tapahtumalista tapahtumalista, TapahtumanTyyppi tyyppi, Jatelaji lavanTyyppi) {
         super(generator, tapahtumalista, tyyppi);
         this.lavanTyyppi = lavanTyyppi;
     }
 
-    //Pura paskat
-    //Tarkista asiakkaan paskat
     @Override
     public void aloitaPalvelu(){  //Aloitetaan uusi palvelu, asiakas on jonossa palvelun aikana
+        saapuneet++;
         Asiakas palveltava = jono.peek();
 		Trace.out(Trace.Level.INFO, "Aloitetaan uusi palvelu asiakkaalle " + palveltava.getId());
 		varattu = true;
 		double palveluaika = generator.sample();
+        // Palveltavan asiakkaan jäte
         Jate j = palveltava.getJatteet().removeFirst();
         System.out.println("LAJITELTU JATE: " + j);
         System.out.println(palveltava.getJatteet().toString());
