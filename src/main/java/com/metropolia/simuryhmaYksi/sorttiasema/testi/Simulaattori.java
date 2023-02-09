a36c7122d1247d62454d264393360b0c8d01e06a
Samu <65758544+SmuuSka@users.noreply.github.com>
2023-02-09 13:57:20 +0200

    Poistettu turha Simulaattori.java

--- src/main/java/com/metropolia/simuryhmaYksi/sorttiasema/testi/Simulaattori.java
+++ src/main/java/com/metropolia/simuryhmaYksi/sorttiasema/testi/Simulaattori.java
@@ -3,21 +3,21 @@ import com.metropolia.simuryhmaYksi.sorttiasema.simu.controller.IKontrolleriMtoV
 import com.metropolia.simuryhmaYksi.sorttiasema.simu.framework.*;
 import com.metropolia.simuryhmaYksi.sorttiasema.simu.model.OmaMoottori;
 
-public class Simulaattori { //Tekstipohjainen
-
-	public static void main(String[] args) {
-		Trace.setTraceLevel(Trace.Level.INFO);
-		Moottori m = new OmaMoottori(new IKontrolleriMtoV() {
-            @Override
-            public void naytaLoppuaika(double aika) {
-
-            }
-            @Override
-            public void visualisoiAsiakas() {
-
-            }
-        });
-		m.setSimulointiaika(50);
-		m.start();
-	}
-}
+//public class Simulaattori { //Tekstipohjainen
+//
+//	public static void main(String[] args) {
+//		Trace.setTraceLevel(Trace.Level.INFO);
+//		Moottori m = new OmaMoottori(new IKontrolleriMtoV() {
+//            @Override
+//            public void naytaLoppuaika(double aika) {
+//
+//            }
+//            @Override
+//            public void visualisoiAsiakas() {
+//
+//            }
+//        });
+//		m.setSimulointiaika(50);
+//		m.start();
+//	}
+//}
