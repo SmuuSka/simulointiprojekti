c158595c38ee0ae548cff5b0ed943e5a9d16f458
Kaspaaro <kaspar.tullus@gmail.com>
2023-02-14 14:25:48 +0200

    Toimii aika inputilla.

--- src/main/java/com/metropolia/simuryhmaYksi/sorttiasema/simu/view/ISimulaattoriUI.java
+++ src/main/java/com/metropolia/simuryhmaYksi/sorttiasema/simu/view/ISimulaattoriUI.java
@@ -4,7 +4,14 @@ public interface ISimulaattoriUI {
     public int[] getVaihteluvali();
     public double getAika();
     public long getViive();
-
+    public double getAsiakasJäteMin();
+    public double getAsiakasJäteMax();
+    public double getAsiakasKgPerSekunti();
+    public int getJatelajiProsenttiELEKTRO();
+    public int getJatelajiProsenttiPALAVA();
+    public int getJatelajiProsenttiPALAMATON();
+    public int getRuuhkaAika();
+    public int getStrategiaTapahtumat();
     public void setLoppuaika();
     public IVisualisointi getVisualisointi();
 }
